249537288f4f496a3278a944b1fad02f37afb383
chanceCoderByPassion <coder.venture@gmail.com>
2024-09-29 19:24:53 +0530

    fucntion stub created

---
+++ password_generator.py
@@ -0,0 +1,8 @@
+from random import random
+def p_gen(characters):
+    special_chars = ['!','@','#','$','%']
+    alphabets = ['ABCDEFGHIJKLMNOPQRSTUVWXYZ']
+    digits = ['0123456789']
+    print(random.randint)
+    for c in range(characters):
+        
